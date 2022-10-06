67fcaebfb6f9904b746477bed0b106a2310ff732
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 16:50:39 +0800

    integrate scss

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
-import sveltePreprocess from 'svelte-preprocess'
+import sveltePreprocess from "svelte-preprocess";
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: [
+    sveltePreprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    }),
+  ],
+};
