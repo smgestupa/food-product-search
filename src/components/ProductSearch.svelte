c886801c0d0b133c1dfdbcde7200aca37f5018a1
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 21:20:02 +0800

    allow disabling of input

--- src/components/ProductSearch.svelte
+++ src/components/ProductSearch.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    export let productTerm: string = "";
+    export let productTerm: string = "", disableInput: boolean = false;
 </script>
 
 <div>
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#9AA0A6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
     <input type="text" 
     placeholder="Search products here"
-    bind:value={ productTerm }>
+    bind:value={ productTerm }
+    disabled={ disableInput }>
 </div>
 
 <style lang="scss">
