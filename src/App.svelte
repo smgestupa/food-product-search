4bf1aa241fda9f13d650600d5172b380f80b968c
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 17:51:13 +0800

    build nav component, place index page

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Route } from "tinro";
+  import Nav from "@components/Nav.svelte";
+  import Index from "@routes/Index.svelte";
+</script>
+
+<Nav/>
+
+<Route path="/">
+  <Index/>
+</Route>
