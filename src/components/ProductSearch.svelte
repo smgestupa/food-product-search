1e1600d874f54c48c492fd6be252ffaf13ca1c92
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 20:24:04 +0800

    add footer semantic, update search box

---
+++ src/components/ProductSearch.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    export let productTerm: string = "";
+</script>
+
+<div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#9AA0A6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+    <input type="text" 
+    placeholder="Search products here"
+    bind:value={ productTerm }>
+</div>
+
+<style lang="scss">
+    div {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        max-width: 720px;
+        height: 44px;
+        padding: 0 1rem;
+        background-color: white;
+        border: 2px solid #DFE1E5;
+        border-radius: 24px;
+        transition-duration: 300ms;
+
+        & > svg {
+            margin-right: 13px;
+            transition-duration: 300ms;
+        }
+
+        & > input {
+            width: 100%;
+            height: 100%;
+            outline: none;
+            background-color: transparent;
+            color: black;
+            font-size: 0.875rem; /* 14px */
+            line-height: 1.25rem; /* 20px */
+
+            &::placeholder {
+                color: #9AA0A6;
+            }
+        }
+
+        &:hover, &:focus-within {
+            border-color: #059669;
+
+            & > svg {
+                stroke: #059669;
+            }
+        }
+    }
+</style>
---
+++ src/semantics/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+    <p>If you liked this, check out the <a href="https://nwahs.vercel.app/#projects">others I've also made</a>.</p>
+</footer>
+
+<style lang="scss">
+    footer {
+        width: 100%;
+        height: 55px;
+        text-align: center;
+        font-size: 0.875rem; /* 14px */
+        line-height: 1.25rem; /* 20px */
+
+        & > p > a {
+            color: #FFE500;
+            font-weight: 600;
+            transition-duration: 300ms;
+
+            &:hover {
+                color: #fff7b3;
+            }
+        }
+    }
+</style>
