f4d10bd51a0c696ae080ffae1f75df471153177a
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-07 07:59:54 +0800

    use no-drop cursor if disabled

--- src/components/ProductSearch.svelte
+++ src/components/ProductSearch.svelte
@@ -2,7 +2,7 @@
     export let productTerm: string = "", disableInput: boolean = false;
 </script>
 
-<div>
+<div class:disabled={ disableInput }>
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#9AA0A6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
     <input type="text" 
     placeholder="Search products here"
@@ -48,6 +48,18 @@
             & > svg {
                 stroke: #059669;
             }
+        }
+    }
+    
+    .disabled {
+        cursor: no-drop;
+
+        & > svg, input {
+            cursor: no-drop;
+        }
+
+        & > input {
+            color: #9AA0A6;
         }
     }
 </style>
