df58f6877689c118a34ece01c1f114af13375b35
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 19:31:04 +0800

    move nav to semantics folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Route } from "tinro";
-  import Nav from "@components/Nav.svelte";
+  import Nav from "@semantics/Nav.svelte";
   import Index from "@routes/Index.svelte";
 </script>
 
