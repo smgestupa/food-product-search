a7cd82c51973d85443a3c9ac003925d943f685af
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-06 16:49:30 +0800

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+
+  resolve: {
+    alias: {
+      "@assets": resolve(__dirname, "./src/assets"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@routes": resolve(__dirname, "./src/routes"),
+      "@semantics": resolve(__dirname, "./src/semantics"),
+    }
+  }
 })
