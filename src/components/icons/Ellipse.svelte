ad742eec176a55997c3dea081d77a10ecc263639
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-10-08 17:50:31 +0800

    add ellipse svg

---
+++ src/components/icons/Ellipse.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let width: string = "24", height: string = "24";
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" { width } { height }><path d="M256 464c-114.69 0-208-93.31-208-208S141.31 48 256 48s208 93.31 208 208-93.31 208-208 208z"/></svg>
